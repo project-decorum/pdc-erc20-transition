bdf39440ab168bd2e0de849de66ffcf0cc326e55
Benno Zeeman <bzeeman@live.nl>
2018-04-17 09:37:54 +0200

    Added functionality to convert ETH to BTC address

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>
+      <label for="eth">Ethereum address: </label>
+      <input type="text" id="eth" v-model="eth_address" required>
+    </div>
+    <div>
+      <label for="btc">Bitcoin address: </label>
+      <input type="text" id="btc" v-model="btc_address" disabled>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import bitcore from 'bitcore-lib';
+import web3 from 'web3';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+function eth_to_btc(eth_address_str: string) {
+    const eth_address_buf = Buffer.from(eth_address_str, 'hex');
+    return bitcore.Address.fromPublicKeyHash(eth_address_buf).toString();
+}
+
+
+@Component({})
+export default class App extends Vue {
+  eth_address: string = '';
+  btc_address: string = '';
+
+  @Watch('eth_address')
+  onInput() {
+    const valid = web3.utils.isAddress(this.eth_address);
+
+    if (!valid) {
+      this.btc_address = '';
+      return;
+    }
+
+    const eth_address_str = this.eth_address.substring(2);
+    this.btc_address = eth_to_btc(eth_address_str);
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -2,3 +2,17 @@ declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
 }
+
+declare module 'bn.js' {
+  interface BigNumber {}
+}
+
+declare module 'web3' {
+  var x: any;
+  export = x;
+}
+
+declare module 'bitcore-lib' {
+  var x: any;
+  export = x;
+}
