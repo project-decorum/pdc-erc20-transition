d7beb5a474b523e1638badcb661a8dd96ca3c075
Benno Zeeman <bzeeman@live.nl>
2018-05-09 18:55:26 +0200

    Removed unnecessary imports

--- src/views/S2.vue
+++ src/views/S2.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class S2 extends Vue {
--- src/views/S3.vue
+++ src/views/S3.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class S3 extends Vue {
